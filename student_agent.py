eb3bb757b2b303aa37b7bd1459bbd889316c3251
AkizumiFox <akizumi.fox@gmail.com>
2025-04-11 16:49:44 +0800

    64000, it=10000,exp=0,v=5000

--- student_agent.py
+++ student_agent.py
@@ -242,7 +242,7 @@ def get_action(state, score):
     legal_moves = [a for a in range(4) if env.is_move_legal(a)]
     if not legal_moves: return random.choice([0, 1, 2, 3])
     
-    mcts_solver = MCTS(env, approximator, iterations=1000, value_norm=20000)
+    mcts_solver = MCTS(env, approximator)
     best_action = mcts_solver.search()
     
     return best_action
